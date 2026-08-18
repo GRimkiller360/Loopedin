2329ad9f2924f4adba4be72afe08561514d8a744
EthanLindeque <137783014+EthanLindeque@users.noreply.github.com>
2026-08-18 10:18:27 +0200

    Fix invisible captions: font size 16 was sized for old default PlayRes, not the real 1080x1920 canvas

--- pipeline/assemble.py
+++ pipeline/assemble.py
@@ -73,7 +73,7 @@ WrapStyle: 2
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Arial,16,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,1,0,0,0,100,100,0,0,1,2,0,2,60,60,120,1
+Style: Default,Arial,90,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,1,0,0,0,100,100,0,0,1,6,0,2,60,60,120,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
